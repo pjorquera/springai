264136bc057cb215120b075672ecd42e7866ffcb
Pedro Jorquera <pedrojorquera@gmail.com>
2025-10-07 16:56:19 +0200

    Added tools

--- src/main/java/com/pjorquera/demo/Config.java
+++ src/main/java/com/pjorquera/demo/Config.java
@@ -2,13 +2,35 @@ package com.pjorquera.demo;
 
 import java.util.Scanner;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+class Tools {
+
+    @Tool(description = "Schedule an appointment for a given person")
+    void scheduleAppointment(String person) {
+        System.out.println("Scheduled an appointment for " + person);
+    }
+
+}
+
 @Configuration
 public class Config {
     
+    private final ChatClient chatClient;
+
+    public Config(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        chatClient = chatClientBuilder
+            .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+            .defaultTools(new Tools())
+            .build();
+    }
+
     @Bean
     ApplicationRunner applicationRunner() {
         return _ -> {
@@ -19,6 +41,15 @@ public class Config {
                     if ("exit".equalsIgnoreCase(input)) {
                         break;
                     }
+                    System.out.println();
+                    System.out.print("AI: ");
+                    chatClient.prompt()
+                        .user(input)
+                        .stream()
+                        .content()
+                        .doOnNext(System.out::print)
+                        .blockLast();
+                    System.out.println();
                 }
             }
         };
