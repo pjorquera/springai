5ad3f3c4e328baecc32b7bd8f4e9f21e2e5dfe32
Pedro Jorquera <pedrojorquera@gmail.com>
2025-10-07 15:45:43 +0200

    Added application runner

---
+++ src/main/java/com/pjorquera/demo/Config.java
@@ -0,0 +1,28 @@
+package com.pjorquera.demo;
+
+import java.util.Scanner;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    
+    @Bean
+    ApplicationRunner applicationRunner() {
+        return _ -> {
+            try (Scanner scanner = new Scanner(System.in)) {
+                while (true) {
+                    System.out.print("User: ");
+                    String input = scanner.nextLine();
+                    if ("exit".equalsIgnoreCase(input)) {
+                        System.out.println("Exiting application...");
+                        break;
+                    }
+                }
+            }
+        };
+    }
+
+}
